ce9b4c921b4da7e95e5918983dfd595907a7091e
Steffen Vogel <post@steffenvogel.de>
2023-08-19 15:26:17 +0200

    Make repo REUSE compliant
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- piv/piv.go
+++ piv/piv.go
@@ -1,4 +1,7 @@
-package main
+// SPDX-FileCopyrightText: 2023 Steffen Vogel <post@steffenvogel.de>
+// SPDX-License-Identifier: Apache-2.0
+
+package piv
 
 import (
 	"crypto"
---
+++ ykoath/provider.go
@@ -0,0 +1,2 @@
+// SPDX-FileCopyrightText: 2023 Steffen Vogel
+// SPDX-License-Identifier: Apache-2.0
