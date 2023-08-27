23a16a6786b7bb33c7cbe0be1502646edaff6bc5
Steffen Vogel <post@steffenvogel.de>
2023-08-27 16:06:53 +0200

    Fix location of secure enclave code
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- providers/ecdh/se/corefoundation.go
+++ providers/ecdh/se/corefoundation.go
@@ -65,3 +65,45 @@ func newCFDictionary(m map[C.CFTypeRef]C.CFTypeRef) (C.CFDictionaryRef, error) {
 		&C.kCFTypeDictionaryValueCallBacks)
 	return ref, nil
 }
+
+func goError(e interface{}) error {
+	if e == nil {
+		return nil
+	}
+
+	switch v := e.(type) {
+	case C.OSStatus:
+		if v == 0 {
+			return nil
+		}
+		return osStatusError{code: int(v)}
+
+	case C.CFErrorRef:
+		if v == nilCFError {
+			return nil
+		}
+
+		code := int(C.CFErrorGetCode(v))
+		if desc := C.CFErrorCopyDescription(v); desc != nilCFString {
+			defer C.CFRelease(C.CFTypeRef(desc))
+
+			if cstr := C.CFStringGetCStringPtr(desc, C.kCFStringEncodingUTF8); cstr != nil {
+				str := C.GoString(cstr)
+
+				return fmt.Errorf("CFError %d (%s)", code, str)
+			}
+
+		}
+		return fmt.Errorf("CFError %d", code)
+	}
+
+	return fmt.Errorf("unknown error type %T", e)
+}
+
+type osStatusError struct {
+	code int
+}
+
+func (oserr osStatusError) Error() string {
+	return fmt.Sprintf("OSStatus %d", oserr.code)
+}
