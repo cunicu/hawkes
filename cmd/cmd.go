aeec4d30f644973c09c1a22c554c06db31173aff
Steffen Vogel <post@steffenvogel.de>
2023-08-27 16:07:14 +0200

    Add simple CLI tool for testing the secure enclave
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

---
+++ cmd/cmd.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+
+	"cunicu.li/go-skes/providers/ecdh/se"
+)
+
+var (
+	label = flag.String("label", "my-label", "User visible label of the generated key pair")
+	tag   = flag.String("tag", "my-tag", "An internal tag")
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("invalid usage")
+	}
+
+	switch os.Args[1] {
+	case "genkey":
+		pk, err := se.GenerateKeyPair(*label, *tag)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(base64.StdEncoding.EncodeToString(pk))
+
+	case "shared-secret":
+	}
+}
