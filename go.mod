8a11c47edf230af8a409022f4724493efe937ee7
Steffen Vogel <post@steffenvogel.de>
2023-12-04 21:23:20 +0100

    Move implementation of YKOATH protocol to a dedicated package
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
 module cunicu.li/hawkes
 
-go 1.21
+go 1.21.3
 
 require (
+	cunicu.li/go-iso7816 v0.4.0
+	cunicu.li/go-ykoath/v2 v2.0.1
 	github.com/ebfe/scard v0.0.0-20230420082256-7db3f9b7c8a7
 	github.com/google/go-tpm v0.9.0
 	github.com/katzenpost/nyquist v0.0.2
@@ -18,9 +20,9 @@ require (
 	github.com/cloudflare/circl v1.3.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/oasislabs/deoxysii v0.0.0-20190807103041-6159f99c2236 // indirect
-	github.com/oasisprotocol/deoxysii v0.0.0-20220228165953-2091330c22b7 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rfjakob/eme v1.1.2 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	gitlab.com/yawning/bsaes.git v0.0.0-20190805113838-0a714cd429ec // indirect
 	gitlab.com/yawning/slice.git v0.0.0-20190714152416-bc4ae2510529 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- provider/ykoath_test.go
+++ provider/ykoath_test.go
@@ -5,13 +5,14 @@ package provider
 
 import (
 	"crypto/rand"
-	"errors"
 	"log/slog"
 	"os"
-	"strings"
 	"testing"
 
-	"github.com/ebfe/scard"
+	"cunicu.li/go-iso7816"
+	"cunicu.li/go-iso7816/filter"
+	"cunicu.li/go-iso7816/test"
+	"cunicu.li/go-ykoath/v2"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,133 +22,93 @@ func TestYKOATH(t *testing.T) {
 	})
 	slog.SetDefault(slog.New(handler))
 
-	require := require.New(t)
+	withCard(t, func(t *testing.T, card *iso7816.Card) {
+		require := require.New(t)
 
-	ctx, err := scard.EstablishContext()
-	require.NoError(err)
-
-	defer func() {
-		err := ctx.Release()
+		p1, err := newYKOATHProvider(card)
 		require.NoError(err)
-	}()
 
-	card, err := connectFirstCard(ctx, isYubikey)
-	require.NoError(err)
-
-	defer func() {
-		err := card.Disconnect(scard.LeaveCard)
+		p2, err := newFileProvider()
 		require.NoError(err)
-	}()
 
-	p1, err := newYKOATHProvider(card)
-	require.NoError(err)
+		ykp, ok := p1.(*ykoathProvider)
+		require.True(ok)
 
-	p2, err := newFileProvider()
-	require.NoError(err)
+		mp, ok := p2.(*fileProvider)
+		require.True(ok)
 
-	ykp, ok := p1.(*ykoathProvider)
-	require.True(ok)
-
-	mp, ok := p2.(*fileProvider)
-	require.True(ok)
-
-	keys, err := p1.Keys()
-	require.NoError(err)
-	require.Empty(keys)
-
-	secret, err := generateSecret()
-	require.NoError(err)
-
-	id1, err := ykp.CreateKeyFromSecret("test1", secret)
-	require.NoError(err)
-	require.NotNil(id1)
-
-	defer func() {
-		err := p1.DestroyKey(id1)
+		keys, err := p1.Keys()
 		require.NoError(err)
+		require.Empty(keys)
+
+		secret, err := generateSecret()
+		require.NoError(err)
+
+		id1, err := ykp.CreateKeyFromSecret("test1", secret)
+		require.NoError(err)
+		require.NotNil(id1)
+
+		defer func() {
+			err := p1.DestroyKey(id1)
+			require.NoError(err)
+
+			keys, err = p1.Keys()
+			require.NoError(err)
+			require.Empty(keys)
+		}()
+
+		id2, err := mp.CreateKeyFromSecret("test1", secret)
+		require.NoError(err)
+		require.NotNil(id2)
+
+		defer func() {
+			err := p2.DestroyKey(id2)
+			require.NoError(err)
+
+			keys, err = p2.Keys()
+			require.NoError(err)
+			require.Empty(keys)
+		}()
 
 		keys, err = p1.Keys()
 		require.NoError(err)
-		require.Empty(keys)
-	}()
+		require.Len(keys, 1)
+		require.Equal(keys[0], id1)
 
-	id2, err := mp.CreateKeyFromSecret("test1", secret)
-	require.NoError(err)
-	require.NotNil(id2)
+		key1, err := p1.OpenKey(id1)
+		require.NoError(err)
+		require.Equal(key1.ID(), id1)
 
-	defer func() {
-		err := p2.DestroyKey(id2)
+		defer func() {
+			err = key1.Close()
+			require.NoError(err)
+		}()
+
+		key2, err := p2.OpenKey(id2)
+		require.NoError(err)
+		require.Equal(key2.ID(), id2)
+
+		defer func() {
+			err = key2.Close()
+			require.NoError(err)
+		}()
+
+		key1HMAC, ok := key1.(PrivateKeyHMAC)
+		require.True(ok)
+
+		key2HMAC, ok := key2.(PrivateKeyHMAC)
+		require.True(ok)
+
+		challenge := []byte("1234")
+
+		ss1, err := key1HMAC.HMAC(challenge)
 		require.NoError(err)
 
-		keys, err = p2.Keys()
+		ss2, err := key2HMAC.HMAC(challenge)
 		require.NoError(err)
-		require.Empty(keys)
-	}()
 
-	keys, err = p1.Keys()
-	require.NoError(err)
-	require.Len(keys, 1)
-	require.Equal(keys[0], id1)
-
-	key1, err := p1.OpenKey(id1)
-	require.NoError(err)
-	require.Equal(key1.ID(), id1)
-
-	defer func() {
-		err = key1.Close()
-		require.NoError(err)
-	}()
-
-	key2, err := p2.OpenKey(id2)
-	require.NoError(err)
-	require.Equal(key2.ID(), id2)
-
-	defer func() {
-		err = key2.Close()
-		require.NoError(err)
-	}()
-
-	key1HMAC, ok := key1.(PrivateKeyHMAC)
-	require.True(ok)
-
-	key2HMAC, ok := key2.(PrivateKeyHMAC)
-	require.True(ok)
-
-	challenge := []byte("1234")
-
-	ss1, err := key1HMAC.HMAC(challenge)
-	require.NoError(err)
-
-	ss2, err := key2HMAC.HMAC(challenge)
-	require.NoError(err)
-
-	require.Equal(ss1, ss2)
-}
-
-func isYubikey(s string) bool {
-	return strings.Contains(s, "YubiKey")
-}
-
-func connectFirstCard(ctx *scard.Context, filter func(string) bool) (*scard.Card, error) {
-	readers, err := ctx.ListReaders()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, reader := range readers {
-		if !filter(reader) {
-			continue
-		}
-
-		card, err := ctx.Connect(reader, scard.ShareShared, scard.ProtocolAny)
-		if err != nil {
-			return nil, err
-		}
-
-		return card, nil
-	}
-
-	return nil, errors.New("no such card exists")
+		require.Equal(ss1, ss2)
+	})
 }
 
 func generateSecret() ([]byte, error) {
@@ -159,4 +120,21 @@ func generateSecret() ([]byte, error) {
 	}
 
 	return secret, nil
+}
+
+func withCard(t *testing.T, cb func(t *testing.T, card *iso7816.Card)) {
+	test.WithCard(t, filter.IsYubiKey, func(t *testing.T, card *iso7816.Card) {
+		require := require.New(t)
+
+		ykCard, err := ykoath.NewCard(card)
+		require.NoError(err)
+
+		_, err = ykCard.Select()
+		require.NoError(err)
+
+		err = ykCard.Reset()
+		require.NoError(err)
+
+		cb(t, card)
+	})
 }
