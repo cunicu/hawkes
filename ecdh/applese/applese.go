23a9d8ac019ed1d3f6d118a27c061ed1ba1a4ec2
Steffen Vogel <post@steffenvogel.de>
2024-06-15 16:02:08 +0200

    fix: Update copyright years
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- ecdh/applese/applese.go
+++ ecdh/applese/applese.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Steffen Vogel <post@steffenvogel.de>
+// SPDX-FileCopyrightText: 2023-2024 Steffen Vogel <post@steffenvogel.de>
 // SPDX-FileCopyrightText: 2023 Meta Platforms, Inc. and affiliates.
 // SPDX-License-Identifier: Apache-2.0
 
